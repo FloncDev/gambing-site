7dc085b974fa1c85357d05c76013ba6e266c256d
DevFlock <37045958+DevFlock@users.noreply.github.com>
2023-08-30 00:00:11 +0100

    ✅ Add tests for Card::from_short()

--- src/test.rs
+++ src/test.rs
@@ -24,14 +24,37 @@ fn test_card_error() {
     let my_card = Card::new(Suite::CLUBS, Rank::NUMERAL(1));
 
     match my_card {
-        Ok(_) => {panic!()},
+        Ok(_) => panic!(),
         Err(e) => {assert_eq!(e, InvalidArgument::InvalidArgument)}
     };
 
     let my_card = Card::new(Suite::CLUBS, Rank::NUMERAL(10));
 
     match my_card {
-        Ok(_) => {panic!()},
+        Ok(_) => panic!(),
         Err(e) => {assert_eq!(e, InvalidArgument::InvalidArgument)}
+    };
+}
+
+#[test]
+fn test_from_short() {
+    let my_card = Card::from_short(String::from("Kh")).unwrap();
+
+    assert_eq!(my_card, Card::new(Suite::HEARTS, Rank::KING).unwrap());
+
+
+    let my_card = Card::from_short(String::from("7s")).unwrap();
+
+    assert_eq!(my_card, Card::new(Suite::SPADES, Rank::NUMERAL(7)).unwrap());
+
+}
+
+#[test]
+fn test_from_short_error() {
+    let my_card = Card::from_short(String::from(""));
+
+    match my_card {
+        Ok(_) => panic!(),
+        Err(e) => assert_eq!(e, InvalidArgument::InvalidArgument)
     };
 }
